a9782d11c9007b416b9592a1dd419eadbfd5c089
Alex Gordeev <i.alex.gordeev@gmail.com>
2018-10-11 21:49:16 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
-composer install
-bin/cake server -H 0.0.0.0 -p 8000
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
+echo -e "${GREEN}Installing composer{NC}"
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo -e "${RED}ERROR: Invalid composer installer signature${NC}"
+    rm composer-setup.php
+    exit 1
+fi
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+
+echo -e "${GREEN}Installing vendors${NC}"
+php composer.phar install --no-dev
+
+echo -e "${GREEN}Run migrations${NC}"
+
+bin/cake migrations migrate
+
+echo -e "${GREEN}Run seed command{NC}"
+bin/cake migrations seed
+
+echo -e "${GREEN}Done, run the server at http://0.0.0.0:8000/${NC}"
 
